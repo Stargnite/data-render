a804f61231997ff6f9c6a0f0da9f9ad106656c4d
--add <sobayotoby@gmail.com>
2025-04-22 21:23:59 +0100

    made modal responsive

--- src/pages/ServiceDetails.tsx
+++ src/pages/ServiceDetails.tsx
@@ -63,7 +63,7 @@ export default function ServiceDetails({ id, onClose }: ServiceModalProps) {
 
   return (
     <div className="fixed inset-0 bg-black/10 bg-opacity-50 flex items-center text-start justify-center z-50 p-5">
-      <div className="bg-white rounded-lg shadow-lg p-6 max-h-[70vh] max-w-[1000px] w-full relative  overflow-y-auto">
+      <div className="bg-white rounded-lg shadow-lg md:p-6 max-h-[70vh] max-w-[1000px] w-full relative  overflow-y-auto">
         <button
           onClick={onClose}
           className="absolute top-2 right-2 text-2xl font-bold cursor-pointer"
@@ -79,11 +79,11 @@ export default function ServiceDetails({ id, onClose }: ServiceModalProps) {
       </button> */}
 
         <div className="p-6 text-gray-800">
-          <h1 className="text-3xl font-bold mb-6">{service?.name}</h1>
+          <h1 className="text-lg md:text-3xl font-bold mb-6">{service?.name}</h1>
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div>
-              <h2 className="text-xl font-semibold mb-4">
+              <h2 className="text-lg md:text-xl font-semibold pb-5">
                 Service Information
               </h2>
               <table className="w-full">
@@ -123,7 +123,7 @@ export default function ServiceDetails({ id, onClose }: ServiceModalProps) {
             </div>
 
             <div>
-              <h2 className="text-xl font-semibold mb-4">
+              <h2 className="text-lg md:text-xl font-semibold pb-5">
                 Location & Availability
               </h2>
               <table className="w-full">
@@ -171,7 +171,7 @@ export default function ServiceDetails({ id, onClose }: ServiceModalProps) {
           </div>
 
           <div className="mt-6">
-            <h2 className="text-xl font-semibold mb-4">Subscription Details</h2>
+            <h2 className="text-lg md:text-xl font-semibold pb-5">Subscription Details</h2>
             <table className="w-full">
               <tbody>
                 <tr className="border-b">
